8a54a178831ab8564ebc40a75f396089e4affe48
Sudhanshu0391 <79698428+Sudhanshu0391@users.noreply.github.com>
2021-03-20 00:26:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Game from './Components/Game';
+
+class App extends React.Component {
+  render() {
+    return(
+      <Game/>
+    )
+  }
+}
+
+export default App;
